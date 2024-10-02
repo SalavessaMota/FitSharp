be9d01e0ed77c17146e8bac7aacf0817f2731e16
SalavessaMota <nunosalavessa@hotmail.com>
2024-10-02 11:37:43 +0100

    Add EF Core and Identity integration with initial migrations
    Updated `FitSharp.csproj` to include EF Core and ASP.NET Core Identity packages (v5.0.7). Modified `Startup.cs` to configure `DataContext` with SQL Server. Added `appsettings.json` connection string for local SQL Server. Introduced `DataContext.cs` and `User.cs` for custom user entity. Created initial migration `20241002095416_InitDb` for database schema and `20241002100248_AddUsers` for ASP.NET Core Identity tables. Added `DataContextModelSnapshot.cs` for EF Core model snapshot.

---
+++ FitSharp/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using FitSharp.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitSharp.Data
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {            
+        }
+
+        
+    }
+}
---
+++ FitSharp/Data/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace FitSharp.Entities
+{
+    public class User : IdentityUser
+    {
+        [MaxLength(50, ErrorMessage = "The field {0} only can contain {1} characters length.")]
+        public string FirstName { get; set; }
+
+
+        [MaxLength(50, ErrorMessage = "The field {0} only can contain {1} characters length.")]
+        public string LastName { get; set; }
+
+
+        [MaxLength(100, ErrorMessage = "The field {0} only can contain {1} characters length.")]
+        public string Address { get; set; }
+
+
+        [Display(Name = "Full Name")]
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
